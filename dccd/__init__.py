b8c7045eafa446dc3807b8b3b1a453f575eb6626
ArthurBernard <arthur.bernard.92@gmail.com>
2019-03-26 11:02:01 +0100

    1.0.2 release

--- dccd/__init__.py
+++ dccd/__init__.py
@@ -2,35 +2,35 @@
 dccd
 ====
 
-The 'dccd' package contains a main class and four classes to download 
-and update data for each exchange, and a module ``time_tools`` to manage 
+The 'dccd' package contains a main class and four classes to download
+and update data for each exchange, and a module ``time_tools`` to manage
 specific time functions needed for each API.
 
-The four classes to download data are ``FromBinance``, ``FromGDax``, 
-``FromKraken`` and ``FromPoloniex``. All have the same methods and almost 
+The four classes to download data are ``FromBinance``, ``FromGDax``,
+``FromKraken`` and ``FromPoloniex``. All have the same methods and almost
 the same parameters:
 
-- __init__(path, crypto, span, fiat(optional), form(optional)): 
-initialisation with path is the path where save the data (string), 
-crypto is a crypto currency (string) and span is the interval time 
-between each observation in seconds (integer) or can be a string as 
-'hourly', 'daily', etc. (see details on the doc string). The optional 
-parameters are fiat the second currency (default is 'USD' and 'USDT' 
-for poloniex and binance) and form the format to save the data (default 
+- __init__(path, crypto, span, fiat(optional), form(optional)):
+initialisation with path is the path where save the data (string),
+crypto is a crypto currency (string) and span is the interval time
+between each observation in seconds (integer) or can be a string as
+'hourly', 'daily', etc. (see details on the doc string). The optional
+parameters are fiat the second currency (default is 'USD' and 'USDT'
+for poloniex and binance) and form the format to save the data (default
 is 'xlsx').
 
-- import_data(start, end): download data with start and end the timestamp 
-(integer) or the date and time (string as 'yyyy-mm-dd hh:mm:ss'), 
-respectively of the first observation and the last observation (default 
-are special parameters start='last' allow the last data saved and 
-end='now' allow the last observation available). Exclusion: Kraken 
-don't allow the end parameter and provide only the thousand last 
+- import_data(start, end): download data with start and end the timestamp
+(integer) or the date and time (string as 'yyyy-mm-dd hh:mm:ss'),
+respectively of the first observation and the last observation (default
+are special parameters start='last' allow the last data saved and
+end='now' allow the last observation available). Exclusion: Kraken
+don't allow the end parameter and provide only the thousand last
 observations.
 
-- save(form(optional), by(optional)): save the data with form the format 
-of the saved data (default is 'xlsx') and by is the "size" of each 
-saved file (default is 'Y' as an entire year). Exclusion: This optional 
-parameters are in progress, let the default parameter for the moment, 
+- save(form(optional), by(optional)): save the data with form the format
+of the saved data (default is 'xlsx') and by is the "size" of each
+saved file (default is 'Y' as an entire year). Exclusion: This optional
+parameters are in progress, let the default parameter for the moment,
 other are not allow.
 
 - get_data(): returns the data frame without any parameter.
@@ -42,7 +42,7 @@ Method chaining is available.
 
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from . import time_tools
 from .time_tools import *
@@ -57,7 +57,7 @@ from .binance import FromBinance
 from . import poloniex
 from .poloniex import FromPoloniex
 
-#__all__ = time_tools.__all__
+# __all__ = time_tools.__all__
 __all__ = ['time_tools']
 __all__ += exchange.__all__
 __all__ += kraken.__all__
