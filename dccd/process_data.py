1ed94e5d535cc81e950a9cf8846a7f83bd704dd2
ArthurBernard <arthur.bernard.92@gmail.com>
2019-08-12 19:00:31 +0200

    add function to process ohlc and raw orders

--- dccd/process_data.py
+++ dccd/process_data.py
@@ -4,7 +4,7 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-08-06 15:25:49
 # @Last modified by: ArthurBernard
-# @Last modified time: 2019-08-07 18:17:56
+# @Last modified time: 2019-08-12 18:59:50
 
 # Built-in packages
 import time
@@ -18,12 +18,36 @@ import pandas as pd
 __all__ = ['set_marketdepth', 'set_trades']
 
 
+def set_orders(orders, t=None):
+    """ Set a dataframe with list of each order.
+
+    Parameters
+    ----------
+    orders : list
+        Each orders in a list.
+
+    Returns
+    -------
+    pd.DataFrame
+        List of orders as dataframe.
+
+    """
+    if t is None:
+        t = int(time.time())
+
+    # Set dataframe
+    df = pd.DataFrame(orders, dtype=np.float64)
+    df.loc[:, 'timestamp'] = t
+
+    return df
+
+
 def set_marketdepth(book, t=None):
     """ Set a market depth dataframe with list of order books.
 
     Parameters
     ----------
-    orderbook : dict
+    book : dict
         Orderbook as dict, where keys is the price and value is the amount.
 
     Returns
@@ -82,8 +106,8 @@ def set_trades(trades):
 
     Parameters
     ----------
-    orderbook : dict
-        Hiorical trades tick by tick as dict.
+    trades : list
+        Historical trades tick by tick as list.
 
     Returns
     -------
@@ -95,3 +119,42 @@ def set_trades(trades):
     df = pd.DataFrame(trades)
 
     return df.sort_values('tid').reset_index(drop=True)
+
+
+def set_ohlc(trades, ts=60):
+    """ Aggregate and set a dataframe with list of trades.
+
+    Parameters
+    ----------
+    trades : list
+        Historical trades tick by tick as list.
+    ts : int, optional
+        Timestep in seconds to aggregate data, default is 60.
+
+    Returns
+    -------
+    pd.DataFrame
+        Aggregated trades as OHLC, dataframe is indexed by timestamp and
+        columns contains 'open', 'high', 'low', 'close', and 'volume'.
+
+    """
+    df = pd.DataFrame(trades).sort_values('tid').reset_index(drop=True)
+    df.timestamp = df.timestamp / 1000
+    t0, t1 = int(df.timestamp.iloc[0] // ts * ts), int(df.timestamp.iloc[-1])
+    db = pd.DataFrame(columns=['open', 'high', 'low', 'close', 'volume'],
+                      index=range(t0, t1 + ts, ts))
+
+    for t in np.arange(t0, t1 + ts, ts):
+        data = df.set_index('timestamp').loc[t: t + ts, :]
+
+        if data.empty:
+
+            continue
+
+        db.loc[t, 'open'] = data.price.iloc[0]
+        db.loc[t, 'high'] = data.price.max()
+        db.loc[t, 'low'] = data.price.min()
+        db.loc[t, 'close'] = data.price.iloc[-1]
+        db.loc[t, 'volume'] = data.amount.sum()
+
+    return db
