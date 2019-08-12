6e8906aa0d40926f7f19459f3ee10c3466afd232
ArthurBernard <arthur.bernard.92@gmail.com>
2019-08-12 16:50:07 +0200

    rm unused import and fix pep8

--- dccd/gdax.py
+++ dccd/gdax.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-02-13 18:25:19
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-08-12 16:49:46
 
-
-""" 
-GDax exchange class to download data. 
+""" GDax exchange class to download data.
 
 """
 
 # Import built-in packages
-import os
-import pathlib
-import time
 
-# Import extern packages
+# Import third party packages
 import requests
 import json
 
@@ -22,9 +22,10 @@ from dccd.exchange import ImportDataCryptoCurrencies
 
 __all__ = ['FromGDax']
 
+
 class FromGDax(ImportDataCryptoCurrencies):
     """ Class to import crypto-currencies data from the GDax exchange.
-    
+
     Parameters
     ----------
     path : str
@@ -32,9 +33,8 @@ class FromGDax(ImportDataCryptoCurrencies):
     crypto : str
         The abreviation of the crypto-currency.
     span : {int, 'weekly', 'daily', 'hourly'}
-        If str, periodicity of observation. 
-        If int, number of the seconds between each observation. Minimal 
-        span is 60 seconds.
+        If str, periodicity of observation. If int, number of the seconds
+        between each observation. Minimal span is 60 seconds.
     fiat : str
         A fiat currency or a crypto-currency.
     form : {'xlsx', 'csv'}
@@ -51,8 +51,11 @@ class FromGDax(ImportDataCryptoCurrencies):
     References
     ----------
     .. [1] https://docs.pro.coinbase.com/
-    
+
     """
+
+    ImportDataCryptoCurrencies.import_data.__doc__ = _import_data.__doc__
+
     def __init__(self, path, crypto, span, fiat='USD', form='xlsx'):
         if crypto is 'XBT':
             crypto = 'BTC'
@@ -60,20 +63,19 @@ class FromGDax(ImportDataCryptoCurrencies):
             self, path, crypto, span, 'GDAX', fiat, form
         )
         self.pair = crypto + '-' + fiat
-        self.full_path = self.path + '/GDAX/Data/Clean_Data/' 
-        self.full_path += str(self.per) + '/' + str(self.crypto) + str(self.fiat)
-        
-    
+        self.full_path = self.path + '/GDAX/Data/Clean_Data/'
+        self.full_path += self.per + '/' + self.crypto + self.fiat
+
     def _import_data(self, start='last', end='now'):
-        """ Download data from GDax for specific time interval
-        
+        """ Download data from GDax for specific time interval.
+
         Parameters
         ----------
         start : int
             Timestamp of the first observation of you want.
         end : int
             Timestamp of the last observation of you want.
-        
+
         """
         self.start, self.end = self._set_time(start, end)
         param = {
@@ -82,14 +84,17 @@ class FromGDax(ImportDataCryptoCurrencies):
             'granularity': self.span
         }
         r = requests.get(
-            'https://api.gdax.com/products/{}/candles'.format(self.pair), 
+            'https://api.gdax.com/products/{}/candles'.format(self.pair),
             param
         )
         text = json.loads(r.text)
         data = [{
-            'date': float(e[0]), 'open': float(e[3]), 'high': float(e[2]), 
-            'low': float(e[1]), 'close': float(e[4]), 'volume': float(e[5]), 
+            'date': float(e[0]),
+            'open': float(e[3]),
+            'high': float(e[2]),
+            'low': float(e[1]),
+            'close': float(e[4]),
+            'volume': float(e[5]),
             'quoteVolume': float(e[4]) * float(e[5])
         } for e in text]
-        return data #self._sort_data(data)
-    ImportDataCryptoCurrencies.import_data.__doc__ = _import_data.__doc__
+        return data
