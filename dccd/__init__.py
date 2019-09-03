dc45b4dcd7e881a23006cd33e55d809bd09e9dc8
ArthurBernard <arthur.bernard.92@gmail.com>
2019-09-03 22:56:21 +0200

    update __init__

--- dccd/__init__.py
+++ dccd/__init__.py
@@ -4,99 +4,73 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-07-26 16:54:02
 # @Last modified by: ArthurBernard
-# @Last modified time: 2019-08-09 11:12:06
+# @Last modified time: 2019-09-03 17:26:41
 
-"""
-dccd
-====
+""" This is `dccd` package.
 
-The 'dccd' package contains a main class and four classes to download
-and update data for each exchange, and a module ``time_tools`` to manage
-specific time functions needed for each API.
-
-The four classes to download data are ``FromBinance``, ``FromGDax``,
-``FromKraken`` and ``FromPoloniex``. All have the same methods and almost
-the same parameters:
-
-- __init__(path, crypto, span, fiat(optional), form(optional)):
-initialisation with path is the path where save the data (string),
-crypto is a crypto currency (string) and span is the interval time
-between each observation in seconds (integer) or can be a string as
-'hourly', 'daily', etc. (see details on the doc string). The optional
-parameters are fiat the second currency (default is 'USD' and 'USDT'
-for poloniex and binance) and form the format to save the data (default
-is 'xlsx').
-
-- import_data(start, end): download data with start and end the timestamp
-(integer) or the date and time (string as 'yyyy-mm-dd hh:mm:ss'),
-respectively of the first observation and the last observation (default
-are special parameters start='last' allow the last data saved and
-end='now' allow the last observation available). Exclusion: Kraken
-don't allow the end parameter and provide only the thousand last
-observations.
-
-- save(form(optional), by(optional)): save the data with form the format
-of the saved data (default is 'xlsx') and by is the "size" of each
-saved file (default is 'Y' as an entire year). Exclusion: This optional
-parameters are in progress, let the default parameter for the moment,
-other are not allow.
-
-- get_data(): returns the data frame without any parameter.
-
-Method chaining is available.
-
-.. automodule:: dccd
-   :members:
+It allows you to download data (prices, volumes, trades, orderbooks, etc.)
+from crypto-currency exchanges (currently only Binance, Bitfinex, Bitmex, GDAX,
+Kraken and Poloniex).
 
 """
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 # ======= #
 #  Tools  #
 # ======= #
 
-from . import time_tools
-from .time_tools import *
-from . import io_tools
-from .io_tools import *
-from . import process_data
-from .process_data import *
+# from . import time_tools
+# from .time_tools import *
+# from . import io_tools
+# from .io_tools import *
+# from . import process_data
+# from .process_data import *
+from . import tools
+# from .tools import *
 
 # ===== #
 #  New  #
 # ===== #
 
-from . import bitfinex
-from .bitfinex import *
-from . import bitmex
-from .bitmex import *
+from . import continuous_dl
+from .continuous_dl import *
+from . import histo_dl
+from .histo_dl import *
+
+# from . import bitfinex
+# from .bitfinex import *
+# from . import bitmex
+# from .bitmex import *
 
 # ===== #
 #  Old  #
 # ===== #
 
-from . import exchange
-from .exchange import ImportDataCryptoCurrencies
-from . import kraken
-from .kraken import FromKraken
-from . import gdax
-from .gdax import FromGDax
-from . import binance
-from .binance import FromBinance
-from . import poloniex
-from .poloniex import FromPoloniex
+# from . import exchange
+# from .exchange import ImportDataCryptoCurrencies
+# from . import kraken
+# from .kraken import FromKraken
+# from . import gdax
+# from .gdax import FromGDax
+# from . import binance
+# from .binance import FromBinance
+# from . import poloniex
+# from .poloniex import FromPoloniex
 
 # __all__ = time_tools.__all__
-__all__ = ['time_tools']
-__all__ += ['io_tools']
+__all__ = ['date_time']
+__all__ += ['io']
 __all__ += ['process_data']
+# __all__ += tools.__all__
 
-__all__ += bitfinex.__all__
-__all__ += bitmex.__all__
+# __all__ += bitfinex.__all__
+# __all__ += bitmex.__all__
+__all__ += continuous_dl.__all__
+__all__ += histo_dl.__all__
 
-__all__ += exchange.__all__
-__all__ += kraken.__all__
-__all__ += binance.__all__
-__all__ += gdax.__all__
-__all__ += poloniex.__all__
+# __all__ += exchange.__all__
+# __all__ += kraken.__all__
+# __all__ += binance.__all__
+# __all__ += gdax.__all__
+# __all__ += poloniex.__all__
