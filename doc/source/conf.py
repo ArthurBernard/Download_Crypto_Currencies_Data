335524f1c44021d81f403fb57401b284fb5b9dc2
ArthurBernard <arthur.bernard.92@gmail.com>
2026-05-29 17:55:48 +0200

    docs: use original PNG logo (1024x1024 RGBA), keep SVG for favicon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -79,8 +79,8 @@ html_theme_options = {
     "source_repository": "https://github.com/ArthurBernard/Download_Crypto_Currencies_Data/",
     "source_branch": "master",
     "source_directory": "doc/source/",
-    "light_logo": "logo.svg",
-    "dark_logo": "logo_dark.svg",
+    "light_logo": "logo.png",
+    "dark_logo": "logo.png",
 }
 html_title = '{} v{} Reference Guide'.format(project, version)
 html_static_path = ['_static']
